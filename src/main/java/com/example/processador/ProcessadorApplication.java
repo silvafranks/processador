f4f9b70c67d2b286085c67512da9347cc8370833
franklin-silva41 <franklin.oliveira@bandtec.com.br>
2022-10-20 15:41:00 -0300

    jsp teste

--- src/main/java/com/example/processador/ProcessadorApplication.java
+++ src/main/java/com/example/processador/ProcessadorApplication.java
@@ -2,12 +2,19 @@ package com.example.processador;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
-public class ProcessadorApplication {
+@SpringBootApplication(scanBasePackages = "com.example.processador")
+public class ProcessadorApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ProcessadorApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ProcessadorApplication.class, args);
 	}
--- src/main/java/com/example/processador/controler/ContaController.java
+++ src/main/java/com/example/processador/controler/ContaController.java
@@ -1,23 +1,19 @@
 package com.example.processador.controler;
 
-import com.example.processador.model.conta.Conta;
 import com.example.processador.model.conta.dto.ContaCriacaoDto;
-import com.example.processador.model.conta.dto.ContaMutationDto;
 import com.example.processador.model.conta.services.ContaServiceCreate;
 import com.example.processador.model.conta.services.ContaServiceMutation;
 import com.example.processador.model.conta.services.ContaServiceQuery;
-import com.example.processador.model.patrimonio.Patrimonio;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.mapping.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Optional;
 
-@RestController
-@RequestMapping("/conta")
+@Controller
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class ContaController {
 
@@ -27,6 +23,12 @@ public class ContaController {
 
 
     private final ContaServiceMutation contaServiceMutation;
+
+    @RequestMapping("/jsp")
+    public ModelAndView jspTeste(){
+
+        return new ModelAndView("index");
+    }
 
     @PostMapping(value = "/{idCliente}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> criarConta(@PathVariable Integer idCliente,
