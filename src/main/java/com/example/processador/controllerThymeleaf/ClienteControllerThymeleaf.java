ccbaacd51b8287b70e2b802fc7c2618ef3bd8a97
Consultor <you@example.com>
2022-11-11 11:39:13 -0300

    adicionando camanda de visualização de contas

--- src/main/java/com/example/processador/controllerThymeleaf/ClienteControllerThymeleaf.java
+++ src/main/java/com/example/processador/controllerThymeleaf/ClienteControllerThymeleaf.java
@@ -12,14 +12,18 @@ import com.example.processador.model.conta.services.ContaServiceQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
 import java.net.http.HttpClient;
 import java.time.OffsetDateTime;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.TreeMap;
 
 @Controller
 public class ClienteControllerThymeleaf {
@@ -56,21 +60,17 @@ public class ClienteControllerThymeleaf {
     }
 
     @RequestMapping(value = "/home")
-    public String home(ClienteLoginDto clienteLoginDto, Model model) {
+    public String home(HttpSession httpSession,ClienteLoginDto clienteLoginDto, Model model) {
+
         Cliente byEmail = clienteRepository.findByEmail(clienteLoginDto.getEmail());
-
-//        if (byEmail == null) {
-//            throw new RuntimeException("Usuario não existe.");
-//        }
-//        if (!clienteLoginDto.getSenha().equals(byEmail.getSenha())) {
-//            throw new RuntimeException("Credenciais Erradas");
-//        }
-        System.out.println(byEmail);
-
+        if (byEmail==null){
+            model.addAttribute("Cliente",httpSession.getAttribute("Cliente"));
+            return "initial";
+        }
+        httpSession.setAttribute("Cliente",byEmail);
         model.addAttribute("Cliente",byEmail);
         return "initial";
     }
-
 
     @RequestMapping(value = "/cadastrar_conta/{idCliente}")
     public String cadastrarConta(ContaCriacaoDto contaCriacaoDto, @PathVariable Integer idCliente,Model model) {
@@ -83,14 +83,32 @@ public class ClienteControllerThymeleaf {
         clienteLoginDto.setEmail(byId.get().getEmail());
         clienteLoginDto.setSenha(byId.get().getSenha());
 
-        System.out.println(byId.get().getId());
         model.addAttribute("Cliente", byId.get());
         return "initial";
     }
     @RequestMapping(value = "/conta/lista/{idCliente}")
-    public String buscarContasPorClientes(@PathVariable Integer idCliente, Model model){
+    public String buscarContasPorClientes( @PathVariable Integer idCliente, Model model){
         List<Conta> contas = contaServiceQuery.contas(idCliente);
+
         model.addAttribute("contas",contas);
         return "contas";
+    }
+
+
+
+    @GetMapping("/grafico")
+    public String getGrafico(Model model, Integer idCliente){
+        Map<String, Integer> graphData = new TreeMap<>();
+        List<Conta> contas = contaServiceQuery.contas(1);
+
+        for (Conta conta:contas) {
+            graphData.put(conta.getBanco().toString(),conta.getValorDisponivel().intValue());
+        }
+//        graphData.put("2016", 147);
+//        graphData.put("2017", 1256);
+//        graphData.put("2018", 3856);
+//        graphData.put("2019", 19807);
+        model.addAttribute("chartData", graphData);
+        return "chart";
     }
 }
